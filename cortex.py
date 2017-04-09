5a5ecb2eefe3391cee83d6771973bb4a15c4dfec
Niamh Alhyme <breathewithinwithout@gmail.com>
2017-04-09 22:25:11 +0100

    Optimisations for cortex.py

--- cortex.py
+++ cortex.py
@@ -1,75 +1,86 @@
 #!/usr/bin/env python3
 
-import math, cmath
+import math
+import cmath
+import argparse
+import time
 from PIL import Image
 
             
-def get_mapped_pixel(im, src_coord, derived_coord):
-    x,y = derived_coord
-    x = x % im.size[0]
-    y = y % im.size[0]
-    x_int = int(x) 
-    x_frac = x - x_int
-    y_int = int(y)
-    y_frac = y - y_int
-    values = []
-    distances = []
-#if x_int >= 0 and x_int < im.size[0] and y_int >= 0 and y_int < im.size[1]:
-    values.append(im.getpixel((x_int, y_int)))
-    distances.append(math.sqrt((x_frac**2) + (y_frac**2)))
-    values.append(im.getpixel(((x_int + 1) % im.size[0], y_int)))
-    distances.append(math.sqrt(((1 - x_frac)**2) + (y_frac**2)))
-    values.append(im.getpixel((x_int, (y_int + 1) % im.size[1])))
-    distances.append(math.sqrt((x_frac**2) + ((1 - y_frac)**2)))
-    values.append(im.getpixel(((x_int + 1) % im.size[0], (y_int + 1) % im.size[1])))
-    distances.append(math.sqrt(((1 - x_frac)**2) + ((1 - y_frac)**2)))
-    total_distance = sum(distances)
-    if total_distance:
-        channels = []
-        for channel in [0,1,2]:
-            channels.append(round(sum([values[i][channel] *
-                                 (distances[i] / total_distance)
-                                 for i in range(len(values))])))
-        pixel_value = tuple(channels)
-    else:
-        pixel_value = (0,0,0)
-#else:
-#    pixel_value = (0,0,0)
-    return pixel_value
+def get_mapped_pixel(size, imdata, derived_coord):
+    """
+    Return the interpolated pixel value in an image at the given coordinate.
+    """
+    # map coordinates outside of the image back into the image
+    # get the integer and fractional parts of the x and y coordinates
+    x_int, x_frac = divmod(derived_coord[0] % size[0], 1)
+    y_int, y_frac = divmod(derived_coord[1] % size[1], 1)
+    # get the values of the four bounding pixels
+    values = [
+        imdata[int((y_int * size[0]) + x_int)],
+        imdata[int((y_int * size[0]) + ((x_int +1) % size[0]))],
+        imdata[int((((y_int + 1) % size[1]) * size[0]) + x_int)],
+        imdata[int((((y_int + 1) % size[1]) * size[0]) + 
+                     ((x_int + 1) % size[0]))]
+    ]
+    # get the distance of the coord from its four bounding pixels
+    distances = [
+        math.hypot(x_frac, y_frac),
+        math.hypot(1-x_frac, y_frac),
+        math.hypot(x_frac, 1-y_frac),
+        math.hypot(1-x_frac, 1-y_frac)
+
+    ]
+    total_dist = sum(distances)
+    distance_weights = [distances[0] / total_dist,
+                        distances[1] / total_dist,
+                        distances[2] / total_dist,
+                        distances[3] / total_dist]
+    # get the number of channels in the image
+    return (
+        (int(round(
+            (values[0][0] * distance_weights[0]) +
+            (values[1][0] * distance_weights[1]) +
+            (values[2][0] * distance_weights[2]) +
+            (values[3][0] * distance_weights[3])))),
+        (int(round(
+            (values[0][1] * distance_weights[0]) +
+            (values[1][1] * distance_weights[1]) +
+             (values[2][1] * distance_weights[2]) +
+            (values[3][1] * distance_weights[3])))),
+        (int(round(
+            (values[0][2] * distance_weights[0]) +
+            (values[1][2] * distance_weights[1]) +
+            (values[2][2] * distance_weights[2]) +
+            (values[3][2] * distance_weights[3])))))
 
 
-def combine_with(im, source_weight=0.5):
-    def _combine_with(src_im, src_coord, derived_coord):
-        px1 = src_im.getpixel(src_coord)
-        px2 = get_mapped_pixel(im, None, derived_coord)
-        pixel_value = tuple([round((px1[i] * source_weight) +
-                                  (px2[i] * (1 - source_weight)))
-                             for i in [0,1,2]])
-        return pixel_value
-    return _combine_with
-        
 
-
-def derive_image(im, modifier=get_mapped_pixel):
-    # create a new image for the derived image to be wrtten to
-    dest = Image.new("RGB", im.size)
-    # the maximum value of r is the length of the diagonal from the centre of
-    # the image to a corner
+def derive_image(im):
+    """
+    Return an image derived from a source image by mapping the x axis to
+    the log of r and the y axis to phi where r and phi are polar coordinates
+    """
+    im = im.convert("RGB")
+    imdata = list(im.getdata())
+    # max_r is the log of the distance from the centre to a corner
     max_r = math.log1p(math.sqrt(im.size[0]**2 + im.size[1]**2) / 2)
-    # the maximum value of phi is the constant 2*pi
+    # max_phi is the constant 2*pi
     max_phi = 2 * math.pi
-    #r_step = max_r / im.size[0]
     phi_step = max_phi / im.size[1]
     data = []
+    # for each pixel in the source image
     for y in range(im.size[1]):
         for x in range(im.size[0]):
-            xy = complex(x - im.size[0]/2, y - im.size[1]/2)
-            r, phi = cmath.polar(xy)
+            # convert to polar coords
+            r, phi = cmath.polar(complex(x - im.size[0]/2, y - im.size[1]/2))
             if phi < 0:
-                phi = (2*math.pi) + phi
-            deriv_x = (math.log1p(r)/max_r) * im.size[0]
-            deriv_y = (phi/max_phi) * im.size[1]
-            data.append(modifier(im, (x,y), (deriv_x, deriv_y)))
+                phi = max_phi + phi
+            # map x and y 
+            data.append(get_mapped_pixel(im.size, imdata,
+                ((math.log1p(r)/max_r) * im.size[0],
+                (phi/max_phi) * im.size[1])))
+    dest = Image.new("RGB", im.size)
     dest.putdata(data)
     try:
         dest.info["comment"] = im.info["comment"]
@@ -78,14 +89,14 @@ def derive_image(im, modifier=get_mapped_pixel):
     return dest
     
 
-
 if __name__ == "__main__":
-    import sys
-    src_path = sys.argv[1]
-#    combin_path = sys.argv[2]
-    dest_path = sys.argv[2]
-    src_im = Image.open(src_path)
-#    combin_im = Image.open(combin_path)
-    #dest_im = derive_image(src_im, combine_with(combin_im, 0.1))
-    dest_im = derive_image(src_im)
-    dest_im.save(dest_path)
+    parser = argparse.ArgumentParser(
+        description="Convert an image into a form constant mapping of itself.")
+    parser.add_argument("source", help="source path")
+    parser.add_argument("destination", help="destination path")
+    args = parser.parse_args()
+    start = time.time()
+    derive_image(Image.open(args.source)).save(args.destination)
+    end = time.time()
+    print(end - start)
+
